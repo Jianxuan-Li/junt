cc1a6047da4c7f9bb6b5a047b927b568e9313ee2
Jack Li <liujin834@gmail.com>
2023-08-11 09:37:04 -0400

    feat: using fixed extension id

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import Button from '@mui/material/Button'
+import React from "react";
+import Button from "@mui/material/Button";
 
-type Props = {}
+type Props = {};
 
 export default function index({}: Props) {
+  const handleConnect = () => {
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      if (chrome.runtime.lastError) {
+        alert(chrome.runtime.lastError.message);
+        return;
+      }
+      console.log(token);
+      // Use the token.
+    });
+  };
+
   return (
     <div>
-        <Button variant="outlined" onClick={() => {
-            chrome.storage.local.clear()
-            chrome.storage.sync.clear()
-        }} >Clear data</Button>
+      <Button
+        variant="outlined"
+        onClick={() => {
+          chrome.storage.local.clear();
+          chrome.storage.sync.clear();
+        }}
+      >
+        Clear data
+      </Button>
+      <Button variant="outlined" onClick={handleConnect}>
+        Connect to google sheet
+      </Button>
     </div>
-  )
+  );
 }
