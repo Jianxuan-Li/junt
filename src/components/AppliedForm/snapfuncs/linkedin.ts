2310e241695d775a27d034bbe2a08ce82b8b9382
Jack Li <liujin834@gmail.com>
2023-10-16 15:25:28 -0400

    feat: adapt the latest linkedin detail page

--- src/components/AppliedForm/snapfuncs/linkedin.ts
+++ src/components/AppliedForm/snapfuncs/linkedin.ts
@@ -13,17 +13,43 @@ export const snapLinkedin = (): JobPostingMessage => {
     message.company = (companyDiv[0] as HTMLElement).innerText
   }
 
+  if (!message.company) {
+    const companyDiv = document.querySelectorAll('div.job-details-jobs-unified-top-card__primary-description')
+    if (companyDiv && companyDiv.length > 0) {
+      message.company = (companyDiv[0] as HTMLElement).innerText
+    }
+  }
+
+  // find position from linkedin job page
   const positionDiv = document.querySelectorAll('h2.jobs-unified-top-card__job-title')
   if (positionDiv && positionDiv.length > 0) {
     message.title = (positionDiv[0] as HTMLElement).innerText
   }
 
+  if (!message.title) {
+    const positionDiv = document.querySelectorAll('h2.job-details-jobs-unified-top-card__job-title')
+    if (positionDiv && positionDiv.length > 0) {
+      message.title = (positionDiv[0] as HTMLElement).innerText
+    }
+  }
+
+  // find url from linkedin job page
   const urlA = document.querySelectorAll('div.jobs-unified-top-card__content--two-pane a.ember-view')
   if (urlA && urlA.length > 0) {
     const href = (urlA[0] as HTMLElement).getAttribute('href')
     // add hostname and remove query string
     const url = `${window.location.hostname}${href?.split('?')[0]}`
     message.url = url
+  }
+
+  if (!message.url) {
+    const urlA = document.querySelectorAll('div.job-details-jobs-unified-top-card__content--two-pane a.ember-view')
+    if (urlA && urlA.length > 0) {
+      const href = (urlA[0] as HTMLElement).getAttribute('href')
+      // add hostname and remove query string
+      const url = `${window.location.hostname}${href?.split('?')[0]}`
+      message.url = url
+    }
   }
 
   // send to popup
