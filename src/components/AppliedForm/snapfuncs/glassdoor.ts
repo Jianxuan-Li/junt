df07117ae87d2d93ed95de02b254bfbbad8e766b
Jack Li <liujin834@gmail.com>
2023-10-27 12:12:41 -0400

    feat: adapt to new glassdoor listing

--- src/components/AppliedForm/snapfuncs/glassdoor.ts
+++ src/components/AppliedForm/snapfuncs/glassdoor.ts
@@ -20,9 +20,10 @@ export const snapGlassdoor = (): JobPostingMessage => {
       // find job title
       const divs = doms[0].querySelectorAll('div')
       if (divs) {
+        let isMatch = false
+
         divs.forEach((div) => {
           // check is the class name matchs `JobDetails_jobTitle.*`
-          let isMatch = false
           div.classList.forEach((className) => {
             if (className.match(/^JobDetails_jobTitle/)) {
               message.title = (div as HTMLElement).innerText
@@ -69,37 +70,9 @@ export const snapGlassdoor = (): JobPostingMessage => {
       return message
     } // end of snapGlassdoorIndex
 
-    // search page
-    const snapGlassdoorSearch = (): JobPostingMessage => {
-      let message: JobPostingMessage = {
-        message: 'jobSnap',
-        type: 'job-searching-glassdoor',
-      }
-
-      // data-test="jobTitle"
-      const jobTitle = document.querySelectorAll('div[data-test="jobTitle"]')
-      if (jobTitle && jobTitle.length > 0) {
-        message.title = (jobTitle[0] as HTMLElement).innerText
-      }
-
-      // div date-test="employerName"
-      const employerName = document.querySelectorAll('div[data-test="employerName"]')
-      if (employerName && employerName.length > 0) {
-        message.company = (employerName[0] as HTMLElement).firstChild?.textContent || ''
-      }
-
-      message.url = window.location.href
-      chrome.runtime.sendMessage(message).catch(() => {})
-      return message
-    } // end of snapGlassdoorSearch
-
     const currentUrl = window.location.href
-    if (currentUrl.includes('index.htm')) {
+    if (currentUrl.includes('/Job')) {
       return snapGlassdoorIndex()
-    }
-
-    if (currentUrl.includes('-jobs-')) {
-      return snapGlassdoorSearch()
     }
 
     return {
